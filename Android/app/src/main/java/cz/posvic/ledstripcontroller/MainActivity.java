6ca740af15c871733368fcd4b3cdf5d114d0a783
Petr Posvic <petr@posvic.cz>
2015-02-19 19:31:53 +0100

    First commit.

---
+++ Android/app/src/main/java/cz/posvic/ledstripcontroller/MainActivity.java
@@ -0,0 +1,309 @@
+package cz.posvic.ledstripcontroller;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.util.UUID;
+
+public class MainActivity extends ActionBarActivity {
+
+	private static final String TAG = MainActivity.class.getName();
+	private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+	// My bluetooth device (on LED strip) address
+	private static final String ADDRESS = "00:00:12:09:18:48";
+
+	private final int RECEIVE_MESSAGE = 1;
+
+	private BluetoothAdapter mBluetoothAdapter;
+	private BluetoothSocket mBluetoothSocket;
+	private ConnectedThread mConnectedThread;
+
+	private StringBuilder sb = new StringBuilder();
+	private Handler mReceiveHandler;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		final Button butTurnOffStrip = (Button) findViewById(R.id.butTurnOffStrip);
+		final Button butRed = (Button) findViewById(R.id.butRed);
+		final Button butGreen = (Button) findViewById(R.id.butGreen);
+		final Button butBlue = (Button) findViewById(R.id.butBlue);
+		final Button butTurnOnChanging = (Button) findViewById(R.id.butTurnOnChanging);
+		final SeekBar seekSpeed = (SeekBar) findViewById(R.id.seekSpeed);
+
+		butTurnOffStrip.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				bluetoothSend("1");
+				butTurnOffStrip.setEnabled(false);
+				butRed.setEnabled(true);
+				butGreen.setEnabled(true);
+				butBlue.setEnabled(true);
+				butTurnOnChanging.setEnabled(true);
+			}
+		});
+
+		butRed.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				bluetoothSend("1r");
+				butTurnOffStrip.setEnabled(true);
+				butRed.setEnabled(false);
+				butGreen.setEnabled(true);
+				butBlue.setEnabled(true);
+				butTurnOnChanging.setEnabled(true);
+			}
+		});
+
+		butGreen.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				bluetoothSend("1g");
+				butTurnOffStrip.setEnabled(true);
+				butRed.setEnabled(true);
+				butGreen.setEnabled(false);
+				butBlue.setEnabled(true);
+				butTurnOnChanging.setEnabled(true);
+			}
+		});
+
+		butBlue.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				bluetoothSend("1b");
+				butTurnOffStrip.setEnabled(true);
+				butRed.setEnabled(true);
+				butGreen.setEnabled(true);
+				butBlue.setEnabled(false);
+				butTurnOnChanging.setEnabled(true);
+			}
+		});
+
+		butTurnOnChanging.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				bluetoothSend("0");
+				butTurnOffStrip.setEnabled(true);
+				butRed.setEnabled(true);
+				butGreen.setEnabled(true);
+				butBlue.setEnabled(true);
+				butTurnOnChanging.setEnabled(false);
+				seekSpeed.setProgress(5);
+			}
+		});
+
+		seekSpeed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				Log.d(TAG, "-- onProgressChanged(...," + progress + "," + fromUser + ") --");
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				int progress = seekBar.getProgress();
+				Log.d(TAG, "-- onStopTrackingTouch(...) --");
+				Log.d(TAG, "progress = " + progress);
+
+				if (progress == 0) {
+					bluetoothSend("1");
+					butTurnOffStrip.setEnabled(false);
+					butRed.setEnabled(true);
+					butGreen.setEnabled(true);
+					butBlue.setEnabled(true);
+				} else {
+					bluetoothSend("0" + progress);
+					butTurnOffStrip.setEnabled(true);
+					butRed.setEnabled(true);
+					butGreen.setEnabled(true);
+					butBlue.setEnabled(true);
+					butTurnOnChanging.setEnabled(false);
+				}
+			}
+		});
+
+		// --------------------------------------------------------------------
+
+		mReceiveHandler = new Handler() {
+			public void handleMessage(android.os.Message msg) {
+				switch (msg.what) {
+					case RECEIVE_MESSAGE:
+						byte[] buffer = (byte[]) msg.obj;
+						sb.append(new String(buffer, 0, msg.arg1));
+
+						int endOfLineIndex = sb.indexOf("\r\n");
+						if (endOfLineIndex > 0) {
+							Log.d(TAG, "received = " + sb.substring(0, endOfLineIndex));
+							sb.delete(0, sb.length());
+						}
+
+						break;
+				}
+			}
+		};
+
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		// Create bluetooth socket
+		try {
+			mBluetoothSocket = createBluetoothSocket(mBluetoothAdapter.getRemoteDevice(ADDRESS));
+		} catch (IOException e) {
+			Log.e(TAG, e.toString());
+		}
+
+		// Discovery is resource intensive. Make sure it isn't going on
+		// when you attempt to connect and pass your message.
+		mBluetoothAdapter.cancelDiscovery();
+
+		// Establish the connection. This will block until it connects.
+		Log.d(TAG, "connecting...");
+		try {
+			mBluetoothSocket.connect();
+			Log.d(TAG, "connection ok");
+		} catch (IOException e1) {
+			Log.e(TAG, e1.toString());
+			try {
+				mBluetoothSocket.close();
+			} catch (IOException e2) {
+				Log.e(TAG, e2.toString());
+			}
+		}
+
+		mConnectedThread = new ConnectedThread(mBluetoothSocket);
+		mConnectedThread.start();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.d(TAG, "-- onPause() --");
+
+		// Close bluetooth socket
+		try {
+			mBluetoothSocket.close();
+		} catch (IOException e) {
+			Log.e(TAG, e.toString());
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+
+		if (id == R.id.action_settings) {
+			Toast.makeText(MainActivity.this, "Created by Petr Pošvic", Toast.LENGTH_SHORT).show();
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	// ------------------------------------------------------------------------
+	// Bluetooth
+	// ------------------------------------------------------------------------
+
+	private void bluetoothSend(String str) {
+		Log.d(TAG, "-- bluetoothSend(" + str + ") --");
+		mConnectedThread.write(str);
+	}
+
+	private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+		if (Build.VERSION.SDK_INT >= 10){
+			try {
+				final Method m = device
+						.getClass()
+						.getMethod(
+								"createInsecureRfcommSocketToServiceRecord",
+								new Class[] { UUID.class }
+						);
+				return (BluetoothSocket) m.invoke(device, MY_UUID);
+			} catch (Exception e) {
+				Log.e(TAG, "Could not create Insecure RFComm Connection", e);
+			}
+		}
+
+		return  device.createRfcommSocketToServiceRecord(MY_UUID);
+	}
+
+	private class ConnectedThread extends Thread {
+		private final InputStream mmInStream;
+		private final OutputStream mmOutStream;
+
+		public ConnectedThread(BluetoothSocket socket) {
+			InputStream tmpIn = null;
+			OutputStream tmpOut = null;
+
+			// Get the input and output streams, using temp objects because
+			// member streams are final
+			try {
+				tmpIn = socket.getInputStream();
+				tmpOut = socket.getOutputStream();
+			} catch (IOException e) {
+				// Ignore
+			}
+
+			mmInStream = tmpIn;
+			mmOutStream = tmpOut;
+		}
+
+		public void run() {
+			byte[] buffer = new byte[256];
+			int bytes;
+
+			while (true) {
+				try {
+					bytes = mmInStream.read(buffer);
+					mReceiveHandler.obtainMessage(
+							RECEIVE_MESSAGE,
+							bytes, -1, buffer
+					).sendToTarget();
+				} catch (IOException e) {
+					break;
+				}
+			}
+		}
+
+		public void write(String message) {
+			Log.d(TAG, "...Data to send: " + message + "...");
+			byte[] msgBuffer = message.getBytes();
+			try {
+				mmOutStream.write(msgBuffer);
+			} catch (IOException e) {
+				Log.d(TAG, "...Error data send: " + e.getMessage() + "...");
+			}
+		}
+	}
+}
---
+++ Arduino/rgb_strip/rgb_strip.ino
@@ -0,0 +1,278 @@
+#include <SoftwareSerial.h>
+
+// RGB led strip
+#define PIN_B      9
+#define PIN_R      10
+#define PIN_G      11
+
+// Bluetooth
+#define PIN_BT_RX   4
+#define PIN_BT_TX   3
+
+// Sound sensor
+#define PIN_SOUND   A6
+
+SoftwareSerial Bluetooth =  SoftwareSerial(PIN_BT_TX, PIN_BT_RX);
+
+// 0 gentle color replacing
+// 1 single color only
+// 2 single color only
+// 3 turned off
+int mode = 3, nextMode = mode;
+
+int spd = 20, nextSpd = spd;
+
+// Values for next mode
+int nextR = 0, nextG = 0, nextB = 0;
+
+char ch;
+
+void setup() {
+  pinMode(PIN_R, OUTPUT);
+  pinMode(PIN_G, OUTPUT);
+  pinMode(PIN_B, OUTPUT);
+
+  Serial.begin(115200);
+  Bluetooth.begin(19200);
+
+  // Firstly turn on red
+  analogWrite(PIN_R, nextR);
+  analogWrite(PIN_G, nextG);
+  analogWrite(PIN_B, nextB);
+}
+
+void loop() {
+  if (mode == 0) {
+    gentle();
+  } else
+
+  if (mode == 1 || mode == 2 || mode == 3) {
+    check();
+    delay(100);
+  }
+}
+
+void gentle() {
+  Serial.println("g+");
+  fade_in(PIN_G);
+  if (check()) return;
+
+  Serial.println("r-");
+  fade_out(PIN_R);
+  if (check()) return;
+
+  Serial.println("b+");
+  fade_in(PIN_B);
+  if (check()) return;
+
+  Serial.println("g-");
+  fade_out(PIN_G);
+  if (check()) return;
+
+  Serial.println("r+");
+  fade_in(PIN_R);
+  if (check()) return;
+
+  Serial.println("g-");
+  fade_in(PIN_G);
+  if (check()) return;
+
+  // Here is RGB
+
+  Serial.println("r-");
+  fade_out(PIN_R);
+  if (check()) return;
+
+  Serial.println("b -> r");
+  fade_swap(PIN_B, PIN_R);
+  if (check()) return;
+
+  Serial.println("g -> b");
+  fade_swap(PIN_G, PIN_B);
+  if (check()) return;
+
+  Serial.println("b-");
+  fade_out(PIN_B);
+  if (check()) return;
+}
+
+/**
+ * return True for interrupt current mode.
+ */
+boolean check() {
+
+  boolean ret = false;
+  bt_proceed();
+
+  if (mode != nextMode) {
+    Serial.print("set mode ");
+    Serial.print(nextMode);
+    Serial.print(" from mode ");
+    Serial.println(mode);
+
+    mode = nextMode;
+
+    analogWrite(PIN_R, nextR);
+    analogWrite(PIN_G, nextG);
+    analogWrite(PIN_B, nextB);
+
+    ret = true;
+  }
+
+  if (spd != nextSpd) {
+    Serial.print("set speed ");
+    Serial.print(nextSpd);
+    Serial.print(" from speed ");
+    Serial.println(spd);
+
+    spd = nextSpd;
+
+    // Speed change shouldn't interrupt current mode
+    // ret = true;
+  }
+
+  return ret;
+}
+
+void bt_proceed() {
+  while (Bluetooth.available()) {
+    ch = Bluetooth.read();
+
+    if (ch == '0') {
+      nextMode = 0;
+      Bluetooth.println("OK mode 0");
+      Serial.println("Set mode 0");
+
+      // This mode starts with red
+      nextR = 255;
+      nextG = 0;
+      nextB = 0;
+
+      if (Bluetooth.available() && parse_speed(Bluetooth.read())) {
+        // New nextSpd from parse_speed
+      } else {
+        nextSpd = 20;
+      }
+
+      Serial.print("Next speed ");
+      Serial.println(nextSpd);
+    } else
+
+    if (ch == '1') {
+
+      // Change mode for color swap. There are two modes, because
+      // colors change only if modes change.
+      if (mode != 1) {
+        nextMode = 1;
+      } else {
+        nextMode = 2;
+      }
+
+      Bluetooth.println("OK mode 1 or 2");
+      Serial.println("Set mode 1 or 2");
+
+      if (Bluetooth.available() && parse_color(Bluetooth.read())) {
+        // New nextR, nextG, nextB from parse_color
+      } else {
+        nextR = 0;
+        nextG = 0;
+        nextB = 0;
+      }
+
+      Serial.print("Next colors (");
+      Serial.print(nextR);
+      Serial.print(",");
+      Serial.print(nextG);
+      Serial.print(",");
+      Serial.print(nextB);
+      Serial.println(")");
+    } else
+
+    if (parse_color(ch)) {
+      Bluetooth.print("OK ");
+      Bluetooth.println(ch);
+      Serial.print("Set ");
+      Serial.println(ch);
+    }
+
+    Serial.println(ch);
+  }
+}
+
+boolean parse_color(char ch) {
+  Serial.print("-- parse_color(");
+  Serial.print(ch);
+  Serial.println(") --");
+
+  if (ch == 'r') {
+    nextR = 255;
+    nextG = 0;
+    nextB = 0;
+    return true;
+  } else
+
+  if (ch == 'g') {
+    nextR = 0;
+    nextG = 255;
+    nextB = 0;
+    return true;
+  } else
+
+  if (ch == 'b') {
+    nextR = 0;
+    nextG = 0;
+    nextB = 255;
+    return true;
+  }
+
+  return false;
+}
+
+boolean parse_speed(char ch) {
+  Serial.print("-- parse_speed(");
+  Serial.print(ch);
+  Serial.println(") --");
+
+  if (ch == '1') {
+    nextSpd = 1;
+  } else
+
+  if (ch == '2') {
+    nextSpd = 5;
+  } else
+
+  if (ch == '3') {
+    nextSpd = 10;
+  } else
+
+  if (ch == '4') {
+    nextSpd = 15;
+  } else
+
+  if (ch == '5') {
+    nextSpd = 20;
+  }
+}
+
+void fade_in(int pin) {
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(pin, i);
+    delay(spd);
+  }
+}
+
+void fade_out(int pin) {
+  for (int i = 255; i >= 0; i--) {
+    analogWrite(pin, i);
+    delay(spd);
+  }
+}
+
+void fade_swap(int pin1, int pin2) {
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(pin1, 255 - i);
+    analogWrite(pin2, i);
+    delay(spd);
+  }
+}
+
